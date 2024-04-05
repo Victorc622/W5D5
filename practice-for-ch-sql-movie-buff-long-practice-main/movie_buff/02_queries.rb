07779f32995be22a6b51e89ba59253772c8c2231
Victor Cai <victorcai622@gmail.com>
2024-04-05 16:39:45 -0400

    finished p2

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
@@ -32,11 +32,22 @@ def vanity_projects
   # starring actor. Show the movie id, title, and director's name.
 
   # Note: Directors appear in the 'actors' table.
-  
+  Movie
+    .select('id', 'title', 'actors.name')
+    .joins(:actors)
+    .where('director_id = castings.actor_id')
+    .where("castings.ord = 1")
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name, and number of supporting roles.
+  Actor
+    .select('COUNT(*) AS roles', 'id','name')
+    .joins(:castings)
+    .where('castings.ord > 1')
+    .group('id')
+    .order('COUNT(actors.id) DESC')
+    .limit(2)
   
 end
